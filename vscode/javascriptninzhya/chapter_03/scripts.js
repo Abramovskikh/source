17a918973fa639e886ee697c9ccb713ce3cdc3dd
Abramovskikh Andrey <djabramovskih@gmail.com>
2018-05-26 15:20:53 +0300

    Заново

--- vscode/javascriptninzhya/chapter_03/scripts.js
+++ vscode/javascriptninzhya/chapter_03/scripts.js
@@ -23,6 +23,54 @@ function whatever(a, b, c) {
         assert(arguments[i] === i + 1, "значение " + arguments[i] + " аргумента === " + i);
     }
 
+    // Не в строгом режиме, при смене значения параметра arguments меняется значение самого параметра
+    arguments[0] = -1;
+
+    assert(a === -1, "Значение параметра поменялось");
+
 }
 
 whatever(1, 2, 3, 4, 5);
+
+function getThis() { return this; }
+
+assert(getThis() === global, "this в не строгом режиме равно глобальному объекту");
+
+
+function whatsMyContext() {
+    return this;
+}
+
+assert(whatsMyContext() === global, "Контекст this при вызове как функции равен global");
+let getMyThis = whatsMyContext;
+assert(getMyThis() === global, "Контекст this при вызове как функции равен global");
+let objThis = {
+    getMyThis: whatsMyContext
+}
+assert(objThis.getMyThis() === objThis, "Контекст this при вызове как метода равен объекту из которого он вызван");
+
+let puppet = {
+    rules: false
+};
+
+function Emperor() {
+    this.rules = true;
+    // Конструктор вернет объекет puppet!!!
+    return puppet;
+}
+
+let emperor = new Emperor();
+
+assert(emperor === puppet, "Конструктор объекта вернул объект локальный");
+assert(emperor.rules === false, "Прсвоенный объект не знает про свойство");
+
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+// При вызове как функцию не как конструктор в глобальной облати появится v()!
+function fooWithThis() {
+    this.v = function() {
+        return this;
+    };
+}
+fooWithThis();
+console.log(v);
+
