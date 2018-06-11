de9f4e57f482a994b2d72760f809add3e3ab862c
Abramovskikh Andrey <djabramovskih@gmail.com>
2018-06-11 18:20:55 +0300

    Базовая работа над UFO3
    Создание вращение астероида

--- www/UFO3(june10)/scripts.js
+++ www/UFO3(june10)/scripts.js
@@ -15,6 +15,14 @@ let Entities = {
         };
         data.entities.background = background;  // Добавление в набор
 
+        // Создание астероидов
+        let asteroidLocations = [[164, 304], [164, 340], [164, 376]];
+        data.entities.asteroidArray = [];
+        asteroidLocations.forEach(function(location) {
+            data.entities.asteroidArray.push(new Entities.tools.Asteroid(data.sprite,
+                location[0], location[1], 32, 32));
+        });
+
     },
     // Инструменты настройки
     tools: {
@@ -25,7 +33,65 @@ let Entities = {
             this.srcY = srcY;
             this.srcW = srcW;
             this.srcH = srcH;
+        },
+        // Класс для создания объекта астероида
+        Asteroid: function(img, x, y, w, h) {
+            let self = this;
+            this.type = "asteroid";
+            this.sprite = new Entities.tools.Sprite(img, 0, 640, 32, 32);
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+            // Виды анимации для объекта
+            this.asteroidAnimations = {
+                // Вращение
+                spin: {
+                    frames: [ // Кадры с главного спрайта
+                        new Entities.tools.Sprite(img, 0, 640, 32, 32), new Entities.tools.Sprite(img, 32, 640, 32, 32),
+                        new Entities.tools.Sprite(img, 64, 640, 32, 32), new Entities.tools.Sprite(img, 96, 640, 32, 32),
+                        new Entities.tools.Sprite(img, 128, 640, 32, 32), new Entities.tools.Sprite(img, 160, 640, 32, 32),
+                        new Entities.tools.Sprite(img, 192, 640, 32, 32), new Entities.tools.Sprite(img, 224, 640, 32, 32),
+                        new Entities.tools.Sprite(img, 256, 640, 32, 32), new Entities.tools.Sprite(img, 288, 640, 32, 32),
+                        new Entities.tools.Sprite(img, 320, 640, 32, 32), new Entities.tools.Sprite(img, 0, 672, 32, 32),
+                        new Entities.tools.Sprite(img, 32, 672, 32, 32), new Entities.tools.Sprite(img, 64, 672, 32, 32),
+                        new Entities.tools.Sprite(img, 96, 672, 32, 32), new Entities.tools.Sprite(img, 128, 672, 32, 32),
+                        new Entities.tools.Sprite(img, 160, 672, 32, 32), new Entities.tools.Sprite(img, 192, 672, 32, 32),
+                        new Entities.tools.Sprite(img, 224, 672, 32, 32)
+                    ],
+                    currentFrame: 0 // Текущий кадр
+                }
+            };
+            // Состояние в котором находится анимация объекта
+            this.states = {
+                // Вращение
+                spinning: {
+                    animation: function(data) {
+                        if(data.frames % 5 === 4) {
+                            self.sprite = self.asteroidAnimations.spin.frames[
+                                self.asteroidAnimations.spin.currentFrame
+                            ];
+                            if(++self.asteroidAnimations.spin.currentFrame >= self.asteroidAnimations.spin.frames.length)
+                                self.asteroidAnimations.spin.currentFrame = 0;
+                        }
+                    }
+                }
+            }
         }
+    }
+};
+
+/* Менеджер анимаций */
+let Animations = {
+    // Покадровая анимауция
+    update: function(data) {
+        Animations.asteroid(data);
+    },
+    // Объекты анимации
+    asteroid: function(data) {
+        data.entities.asteroidArray.forEach(function(asteroid){
+            asteroid.states.spinning.animation(data);
+        });
     }
 };
 
@@ -38,7 +104,17 @@ let Render = {
     },
     // Покадровая перерисовка
     update: function(data) {
-
+        let context = data.canvas.fCtx;
+        // Очистка экра перед рисованием
+        context.clearRect(0, 0, data.canvas.w, data.canvas.h);
+        // Сохраняем состояние холста
+        context.save();
+        // Рисуем астероиды
+        data.entities.asteroidArray.forEach(function(asteroid){
+            Render.tools.drawSprite(asteroid, context);
+        });
+        // Восстонавливаем состояние холста
+        context.restore();
     },
     // Инструменты для визуализации
     tools: {
@@ -92,10 +168,16 @@ let Game = {
     run: function(data) {
         let loop = function() {
             Game.render(data);
+            Game.animation(data);
             data.frames++;
             window.requestAnimationFrame(loop);
         };
         loop();
+    },
+
+    // Контроль анимации
+    animation: function(data) {
+        Animations.update(data);
     },
 
     // Контроль отображения
